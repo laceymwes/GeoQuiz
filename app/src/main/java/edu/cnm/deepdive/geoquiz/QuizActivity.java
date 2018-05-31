1a00942399f93eb9ad89292eea50046c56f87400
Michael Lacey <laceymwes@gmail.com>
2018-05-31 08:01:17 -0600

    Add previous button and implement onClick interface.

--- app/src/main/java/edu/cnm/deepdive/geoquiz/QuizActivity.java
+++ app/src/main/java/edu/cnm/deepdive/geoquiz/QuizActivity.java
@@ -1,10 +1,12 @@
 package edu.cnm.deepdive.geoquiz;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,10 +14,11 @@ public class QuizActivity extends AppCompatActivity {
 
   private Button mTrueButton;
   private Button mFalseButton;
-  private Button mNextButton;
+  private ImageButton mNextButton;
+  private ImageButton mPrevButton;
   private TextView mQuestionTextView;
 
-  private Question[] mQuestionBank = new Question[] {
+  private Question[] mQuestionBank = new Question[]{
       new Question(R.string.question_australia, true),
       new Question(R.string.question_oceans, true),
       new Question(R.string.question_mideast, false),
@@ -32,23 +35,23 @@ public class QuizActivity extends AppCompatActivity {
     setContentView(R.layout.activity_quiz);
 
     mQuestionTextView = findViewById(R.id.question_text_view);
-    mQuestionTextView.setOnClickListener(new View.OnClickListener(){
+    mQuestionTextView.setOnClickListener(new View.OnClickListener() {
       @Override
-      public void onClick(View v){
+      public void onClick(View v) {
         incrementIndex(); // increment current index
         updateQuestion(); // change question TextView text to next element
       }
     });
 
-    mTrueButton =  findViewById(R.id.true_button);
-    mTrueButton.setOnClickListener(new View.OnClickListener(){
+    mTrueButton = findViewById(R.id.true_button);
+    mTrueButton.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
         checkAnswer(true);
       }
     });
 
-    mFalseButton =  findViewById(R.id.false_button);
+    mFalseButton = findViewById(R.id.false_button);
     mFalseButton.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
@@ -59,25 +62,30 @@ public class QuizActivity extends AppCompatActivity {
     mNextButton = findViewById(R.id.next_button);
     mNextButton.setOnClickListener(new View.OnClickListener() {
       @Override
-      public void onClick(View v){
+      public void onClick(View v) {
         incrementIndex();
         updateQuestion();
       }
     });
+
+    mPrevButton = findViewById(R.id.prev_button);
+    mPrevButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        decrementIndex();
+        updateQuestion();
+      }
+    });
     updateQuestion();
-    }
-    private void updateQuestion(){
-    int question = mQuestionBank[mCurrentIndex].getTextResId();
-    mQuestionTextView.setText(question);
   }
-  private void checkAnswer(boolean userPressedTrue){
+
+  private void checkAnswer(boolean userPressedTrue) {
     boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
     int messageResId = 0;
 
-    if(userPressedTrue == answerIsTrue){
+    if (userPressedTrue == answerIsTrue) {
       messageResId = R.string.correct_toast;
-    }
-    else {
+    } else {
       messageResId = R.string.incorrect_toast;
     }
     Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
@@ -87,8 +95,13 @@ public class QuizActivity extends AppCompatActivity {
     mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
   }
 
-  // FIXME: use in onClick interface for previous button
-  private void decrementIndex(){
-    mCurrentIndex = Math.abs(mCurrentIndex - 1) % mQuestionBank.length;
+  private void decrementIndex() {
+    // if index already at first element make no changes
+    mCurrentIndex = (mCurrentIndex < 1) ? 0 : (mCurrentIndex - 1) % mQuestionBank.length;
+  }
+
+  private void updateQuestion() {
+    int question = mQuestionBank[mCurrentIndex].getTextResId();
+    mQuestionTextView.setText(question);
   }
 }
